b6be53b60f380e3c95be38f6cf43e3573b96ac3b
YuTanaka <YRK00337@nifty.com>
2015-10-01 12:19:31 +0900

    テストとモジュールのスタブを作成

---
+++ src/latlon-distance.js
@@ -0,0 +1,17 @@
+// GSR80による赤道半径(m)
+var EARTH_R = 6378137.0;
+
+/** 2点間の直線距離を返す
+ * @param double lat0 緯度
+ * @param double lon0 経度
+ * @param double ele0 標高
+ * @param double lat1 緯度2
+ * @param double lon1 経度2
+ * @param double ele1 標高2
+ * @return double ２点間の距離をメートル単位で返す
+ */
+module.exports = function(lat0, lon0, ele0, lat1, lon1, ele1) {
+  // Math.sin() / cos()
+  // Math.PI
+  return Math.cos(0.5);
+}
