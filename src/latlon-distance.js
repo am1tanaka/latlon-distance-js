f4b55f57e33b6a214a8bc1ab847840414fffdc22
YuTanaka <YRK00337@nifty.com>
2015-10-01 22:33:38 +0900

    計算とチェック完了

--- src/latlon-distance.js
+++ src/latlon-distance.js
@@ -1,5 +1,6 @@
 // GSR80による赤道半径(m)
-var EARTH_R = 6378137.0;
+// 国土地理院 日本の測地系 http://www.gsi.go.jp/sokuchikijun/datum-main.html
+var EARTH_R = 6378137;
 
 /** 2点間の直線距離を返す
  * @param double lat0 緯度
@@ -8,10 +9,40 @@ var EARTH_R = 6378137.0;
  * @param double lat1 緯度2
  * @param double lon1 経度2
  * @param double ele1 標高2
- * @return double ２点間の距離をメートル単位で返す
+ * @return double ２点間の距離をメートル単位で返す。引数が異常な場合はfalseを返す
  */
 module.exports = function(lat0, lon0, ele0, lat1, lon1, ele1) {
   // Math.sin() / cos()
   // Math.PI
-  return Math.cos(0.5);
+  var pos0 = calcPos(lat0, lon0, ele0);
+  var pos1 = calcPos(lat1, lon1, ele1);
+  if (!pos0 || !pos1) {
+    return false;
+  }
+  var dx = pos0.x-pos1.x;
+  var dy = pos0.y-pos1.y;
+  var dz = pos0.z-pos1.z;
+  return Math.sqrt(dx*dx+dy*dy+dz*dz);
+};
+
+/**
+ * 指定の緯度経度と標高を3D座標に変換して、オブジェクトで返す
+ * @param double lat 緯度
+ * @param double lon 経度
+ * @param double ele 標高(m)
+ * @return x:X座標 / y:Y座標 / z:Z座標。エラーの時はfalse
+ */
+function calcPos(lat, lon, ele) {
+  var ret = {};
+  var r = (EARTH_R+(ele-0));
+  lat = (lat-0)*Math.PI/180;
+  lon = (lon-0)*Math.PI/180;
+  ele = ele-0;
+  if (isNaN(lat) || isNaN(lon) || isNaN(ele)) {
+    return false;
+  }
+  ret.x = r*Math.cos(lon)*Math.cos(lat);
+  ret.y = r*Math.sin(lat);
+  ret.z = r*Math.sin(lon)*Math.cos(lat);
+  return ret;
 }
--- tests/test.js
+++ tests/test.js
@@ -2,22 +2,44 @@ var latlon = require('../src/latlon-distance');
 
 var latlon_test = {
   test1: function(test) {
-    test.expect(5);
+    var dist = 0;
+    var errcheck;
+    test.expect(6);
 
     // 35.632493, 139.438246 > 35.634376, 139.449318 (1.01km)
-    test.equal(latlon(35.632493, 139.438246 ,0, 35.634376, 139.449318, 0), 1010);
+    dist = latlon(35.632493, 139.438246 ,0, 35.634376, 139.449318, 0);
+    test.ok(checkPer(dist, 1010, 5), "OK:"+dist+"/"+1010);
     // 35.634376, 139.449318 > 35.652303, 139.448031 (2km)
-    test.equal(latlon(35.634376, 139.449318, 0, 35.652303, 139.448031, 0), 2000);
+    dist = latlon(35.634376, 139.449318, 0, 35.652303, 139.448031, 0);
+    test.ok(checkPer(dist, 2000, 5), "OK:"+dist+"/"+2000);
     // 35.652303, 139.448031 > 35.620075, 139.409493 (5km)
-    test.equal(latlon(35.652303, 139.448031, 0, 35.620075, 139.409493, 0), 5000);
+    dist = latlon(35.652303, 139.448031, 0, 35.620075, 139.409493, 0);
+    test.ok(checkPer(dist, 5000, 5), "OK:"+dist+"/"+5000);
     // 35.620124, 139.409352 > 35.620124, 139.409331 (1.97m)
-    test.equal(latlon(35.620124, 139.409352, 0, 35.620124, 139.409331, 0), 1.97);
+    dist = latlon(35.620124, 139.409352, 0, 35.620124, 139.409331, 0);
+    test.ok(checkPer(dist, 1.97, 5), "OK:"+dist+"/"+1.97);
     // 35.620125, 139.409331 > 35.620134, 139.409328 (1.06m)
-    test.equal(latlon(35.620125, 139.409331, 0, 35.620134, 139.409328, 0), 1.06);
+    dist = latlon(35.620125, 139.409331, 0, 35.620134, 139.409328, 0);
+    test.ok(checkPer(dist, 1.06, 5), "OK:"+dist+"/"+1.06);
+
+    // 非数値
+    errcheck = latlon("a", 10, 0, null, 139.409328, 0);
+    test.ok(!errcheck, "Error check ok.");
 
     // 終了
     test.done();
   }
 };
+
+/**
+ * 両数値が誤差のパーセンテージ内かをチェックしてtrue / falseで返す
+ * @param double check チェックする値
+ * @param double targ 目的の値
+ * @param double gosa 誤差のパーセンテージ
+ */
+function checkPer(check, targ, gosa) {
+  var rate = (check/targ)*100;
+  return (rate >= 100-gosa) && (rate <= 100+gosa);
+}
 
 module.exports = latlon_test;
